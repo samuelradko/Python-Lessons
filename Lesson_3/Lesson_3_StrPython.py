0a0966fad9da58bdbcbb264efb631a654dceabc5
Samuel Radko <65078021+samuelradko@users.noreply.github.com>
2023-02-02 21:29:27 +0200

    upd

---
+++ Lesson_3/Lesson_3_StrPython.py
@@ -0,0 +1,137 @@
+s = "Frist Answer"
+print(s.title().center(50, "-"))
+# #What is the output of the following code
+print(len("Hello, World!"))
+# # Output: 13
+
+
+s = "Second Answer"
+print(s.title().center(50, "-"))
+# #What is the output of the following code:
+print("Hello, World!"[0:5])
+# # Output: "Hello"
+
+
+
+s = "Third Answer"
+print(s.title().center(50, "-"))
+# #What is the output of the following code:
+text = "Hello, World!"
+print(text.replace("Hello", "Hi"))
+# # Output: "Hi, World!"
+
+
+
+s = "Forth Answer"
+print(s.title().center(50, "-"))
+# #    What is the output of the following code:
+text = "Hello, World!"
+print(text.isdigit())
+# # Output: False
+
+
+
+
+s = "Fifth Answer"
+print(s.title().center(50, "-"))
+# #What is the output of the following code:
+numbers = [1, 2, 3, 4, 5]
+print(sum(numbers))
+# # Output: 15
+
+
+
+
+s = "Sixed Answer"
+print(s.title().center(50, "-"))
+# #What is the output of the following code:
+numbers = [1, 2, 3, 4, 5]
+print(min(numbers))
+# # Output: 1
+
+
+
+s = "Seven Answer"
+print(s.title().center(50, "-"))
+# #What is the output of the following code:
+numbers = [1, 2, 3, 4, 5]
+print(max(numbers))
+# # Output: 5
+
+
+
+s = "eight Answer"
+print(s.title().center(50, "-"))
+# #What is the output of the following code:
+numbers = [1, 2, 3, 4, 5]
+print(numbers[::-1])
+# # Output: [1,2,3,4] << Got this wrong the right asnwer is [5,4,3,2,1]
+
+
+
+s = "Nine Answer"
+print(s.title().center(50, "-"))
+# #What is the output of the following code:
+numbers = [1, 2, 3, 4, 5]
+print(numbers[0:3])
+# # Output: [1,2,3]
+
+
+
+s = "Ten Answer"
+print(s.title().center(50, "-"))
+# #What is the output of the following code:
+numbers = [1, 2, 3, 4, 5]
+numbers.append(6)
+print(numbers)
+# # Output: [1,2,3,4,5,6]
+
+
+
+s = "Eleven Answer"
+print(s.title().center(50, "-"))
+# # What is the output of the following code:
+numbers = [1, 2, 3, 4, 5]
+numbers.insert(0, 0)
+print(numbers)
+# # Output:[0,1,2,3,4,5]
+
+
+
+s = "Twelve Answer"
+print(s.title().center(50, "-"))
+# # What is the output of the following code:
+numbers = [1, 2, 3, 4, 5]
+numbers.remove(3)
+print(numbers)
+# # Output: [1,2,4,5]
+
+
+
+
+s = "Thirten Answer"
+print(s.title().center(50, "-"))
+# #    What is the output of the following code:
+numbers = [1, 2, 3, 4, 5]
+numbers.pop()
+print(numbers)
+# #Output: [1,2,3,4]
+
+
+
+s = "Forthin Answer"
+print(s.title().center(50, "-"))
+# #    What is the output of the following code:
+numbers = [1, 2, 3, 4, 5]
+numbers.clear()
+print(numbers)
+# #Output: []
+
+
+
+s = "Fifthen Answer"
+print(s.title().center(50, "-"))
+# #    What is the output of the following code:
+numbers = [1, 2, 3, 4, 5]
+print(len(numbers))
+# #Output: 4 << got this wrong the right answer is 5
---
+++ Lesson_3/Lesson_3_StrPythonPT2.py
@@ -0,0 +1,78 @@
+s = "[A1]"
+print(s.title().center(50, "-"))
+#     Write a program to convert the first letter of a string to uppercase.
+a1 = "hello python"
+print(a1.capitalize())
+
+#     Write a program to count the number of occurrences of a substring in a string.
+s = "[A2]"
+print(s.title().center(50, "-"))
+
+a2 = "Hello there good cat, you are good cat really good cat"
+print(a2.count("good"))
+
+#     Write a program to check if a string starts with a specific substring.
+s = "[A3]"
+print(s.title().center(50, "-"))
+a3 = "Great Ben is Geat!"
+print(a3.startswith("Great"))
+
+#     Write a program to check if a string ends with a specific substring.
+s = "[A4]"
+print(s.title().center(50, "-"))
+a4 = "If a woodchuck can chuck wood, does it really there?"
+print(a4.endswith("there?"))
+
+#     Write a program to find the index of a substring in a string.
+s = "[A5]"
+print(s.title().center(50, "-"))
+a5 = "Mary had a little lamb little lamb."
+print(a5.find("Mary"))
+
+#     Write a program to split a string into a list based on a specific character.
+s = "[A6]"
+print(s.title().center(50, "-"))
+a6 = "If you remove all the the then the will be gone."
+print(a6.split())
+
+#     Write a program to replace a specific substring in a string.
+s = "[A7]"
+print(s.title().center(50, "-"))
+a7 = "If you replace all the the, the will be funny."
+print(a7.replace("the", "funny"))
+
+#     Write a program to check if a string is alphanumeric.  ##  had to research to answer this question ##
+s = "[A8]"
+print(s.title().center(50, "-"))
+a8 = "Blink21"
+print(a8.isalnum())
+
+#     Write a program to check if a string is alphabetic.  ## had to research to answer this question ##
+s = "[A9]"
+print(s.title().center(50, "-"))
+a9 = "asdasda"
+print(a9.isalpha())
+
+#     Write a program to check if a string is numeric.  ## had to research to answer this question ##
+s = "[A10]" 
+print(s.title().center(50, "-"))
+a10 = "123451231"
+print(a10.isnumeric())
+
+#     Write a program to check if a string is lowercase.
+s = "[A11]"
+print(s.title().center(50, "-"))
+a11 = "a1b2c3d4e5f6g7."
+print(a11.islower())
+
+#     Write a program to check if a string is uppercase.  
+s = "[A12]"
+print(s.title().center(50, "-"))
+a12 = "ABCDEFG."
+print(a12.isupper())
+
+#     Write a program to strip whitespace from the beginning and end of a string.
+s = "[A13]"
+print(s.title().center(50, "-"))
+a13 = "  a1b2c3d4e5f6g7    "
+print(a13.strip())
